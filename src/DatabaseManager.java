dffab1cd07fd39b9176549db56ce549d45f2ae14
joshua <laverjoshua@gmail.com>
2019-12-03 20:53:14 -0400

    feat: Start on DatabaseManager

--- src/DatabaseManager.java
+++ src/DatabaseManager.java
@@ -1,8 +1,51 @@
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class DatabaseManager {
+  public static Connection Connector() {
+    Connection connection = null;
+    try{
+      Class.forName("org.sqlite.JDBC");
+      connection  = DriverManager.getConnection("jdbc:sqlite:todo");
+    } catch(Exception e){
+      System.out.println(e);
+    }
+    return connection;
+  }
 
 
 
 
 
+  public static void main(String []args) throws Exception{
+    Connection connection = Connector();
+    System.out.println(connection);
+
+
+    System.out.println("Show Tables");
+
+    DatabaseMetaData metaData = connection.getMetaData();
+
+    Statement statement = connection.createStatement();
+
+//    ResultSet set1 = statement.executeQuery("insert")
+
+    ResultSet set = statement.executeQuery("select * from board");
+
+
+    while(set.next())
+      System.out.println(set.getString("object"));
+
+    ResultSet tables = metaData.getTables(null, null, "%", new String[] {"TABLE"});
+    System.out.println("Output");
+
+    while (tables.next()) {
+      System.out.println(tables.getString("TABLE_NAME"));
+    }
+  }
 }
+
+
